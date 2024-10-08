7eba5a6672b976fb91a2029fc8844cb60dba97f5
naufalmuhdaffa <naufalmuhdaffa@gmail.com>
2024-10-08 11:14:01 +0700

    update comment

--- Praktikum2.py
+++ Praktikum2.py
@@ -1,13 +1,17 @@
+# 1. mencetak/menampilkan nama (Naufal Muhammad Daffa)
 print("Naufal Muhammad Daffa")
 
+# 2. menampilkan output I am learning Python basics.
 print("I am learning Python basics.")
 
+# 3. deklarasi variabel Name, diikuti outputnya  
 Name = "Naufal Muhammad Daffa"
+# menampilkan "Hello," beserta variabel Name
 print('Hello,'+Name)
-
-# baris 1: deklarasi variabel Name, diikuti outputnya, baris 2: menampilkan "Hello," beserta variabel Name  
 
 """
 Baris 1: Deklarasi variabel age, meminta user menginputkan age.
 Baris 2: Menampilkan variable age.
 """
+age = input("How old are you? ")
+print(age)
