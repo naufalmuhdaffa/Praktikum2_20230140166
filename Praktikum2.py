ae3372fb859860debd9f00a60cdc43c231cd16ce
naufalmuhdaffa <naufalmuhdaffa@gmail.com>
2024-10-08 10:32:52 +0700

    membuat project baru

---
+++ Praktikum2.py
@@ -0,0 +1,13 @@
+print("Naufal Muhammad Daffa")
+
+print("I am learning Python basics.")
+
+Name = "Naufal Muhammad Daffa"
+print('Hello,'+Name)
+
+# baris 1: deklarasi variabel Name, diikuti outputnya, baris 2: menampilkan "Hello," beserta variabel Name  
+
+"""
+Deklarasi variabel age, meminta user menginputkan age.
+Menampilkan variable age.
+"""
